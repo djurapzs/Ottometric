56169b4ab849c197bcc78f6ba3a532b0c112d046
djuro <djurapez@gmail.com>
2025-07-12 21:50:38 +0200

    add test: should validate if total value corresponds with row sums

---
+++ cypress/e2e/lanes-sum.spec.ts
@@ -0,0 +1,31 @@
+// cypress/e2e/lanes-sum.spec.ts
+
+import { assertTotalIsValid } from "../support/assertations/assert-total-is-valid";
+import { CameraPrograms } from "../support/enums/camera-programs.enum";
+import {
+  calculateColumnAverages,
+  getFooterValues,
+} from "../support/helpers/calculate-column-average";
+import { stubPosthogArray } from "../support/intercepts/pre-login";
+import kpiFcmLanesPage from "../support/pages/kpi-fcm-lanes-page";
+
+describe("Check if the sum of values from each row corresponds with the value from the total row", () => {
+  beforeEach(() => {
+    stubPosthogArray();
+  });
+  it("should validate if total value corresponds with row sums", () => {
+    cy.visit("/");
+    cy.login();
+    cy.selectProgram(CameraPrograms.VT1);
+    cy.goToKpiLanes();
+
+    getFooterValues(kpiFcmLanesPage.centerTable).then((footerValues) => {
+      calculateColumnAverages(
+        kpiFcmLanesPage.centerTable,
+        cy.wrap(footerValues)
+      ).then((averages) => {
+        assertTotalIsValid(footerValues, averages);
+      });
+    });
+  });
+});

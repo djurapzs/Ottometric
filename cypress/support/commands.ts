d8d75fd04448d9b2253098c39f9f386806a95d4c
djuro <djurapez@gmail.com>
2025-07-12 11:53:40 +0200

    add command for login

---
+++ cypress/support/commands.ts
@@ -0,0 +1,23 @@
+import "./pages/login-page";
+import "./pages/kpi-page";
+
+declare global {
+  // Extend Cypress' Chainable interface to include custom commands
+  // This allows us to use these commands in our tests without TypeScript errors
+  namespace Cypress {
+    interface Chainable {
+      login(): Chainable<void>;
+      selectProgram(name: string): Chainable<void>;
+      goToKpiLanes(): Chainable<void>;
+    }
+  }
+}
+
+Cypress.Commands.add("login", () => {
+  const user = Cypress.env("user");
+  const pass = Cypress.env("pass");
+  cy.visit("/");
+  cy.get('input[type="email"]').type(user);
+  cy.get('input[type="password"]').type(pass);
+  cy.get('button[type="submit"]').click();
+});

9b9ef2fa55d9b78adcec46c6a632c656718dff0b
djuro <djurapez@gmail.com>
2025-07-13 09:41:11 +0200

    add count-fn-events test final

--- cypress/e2e/count-fn-events.spec.ts
+++ cypress/e2e/count-fn-events.spec.ts
@@ -1,9 +1,10 @@
 // cypress/e2e/lanes-sum.spec.ts
-
+import { Zone1 } from "../support/enums/zone1.enum";
 import { CameraPrograms } from "../support/enums/camera-programs.enum";
 import { followRedirectionAndVisit } from "../support/helpers/handle-redirection";
 import { stubPosthogArray } from "../support/intercepts/pre-login";
 import { redirectRequest } from "../support/intercepts/redirect-request";
+import kpiDetailsPage from "../support/pages/kpi-details-page";
 import tablePage from "../support/pages/table-page";
 
 describe("Count the FN events in the timeline.", () => {
@@ -18,9 +19,15 @@ describe("Count the FN events in the timeline.", () => {
     cy.selectProgram(CameraPrograms.VI1);
     cy.goToKpiZone1();
 
-    tablePage.DTIDmultiSelect(2);
+    tablePage.DTIDmultiSelect(7);
     tablePage.seeDetailsButton.should("be.enabled").click();
 
     followRedirectionAndVisit("VI1", "zone1");
+
+    kpiDetailsPage.selectZone1Value(Zone1.FN);
+
+    kpiDetailsPage.getTotalEventCount().then((count) => {
+      console.log("Total FN events count:", count);
+    });
   });
 });
